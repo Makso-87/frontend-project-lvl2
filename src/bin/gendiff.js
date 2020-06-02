496b682181b95c7322b07e820d4a885f5b5375d6
Makso-87 <makso16108@gmail.com>
2020-06-02 20:47:09 +0300

    венул старое название проекта

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,12 +5,12 @@ import { program } from 'commander';
 program
     .version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
-    .arguments('<filepath1> <filepath2>')
-    .action(function (filepath1, filepath2) {
-        filepath1Value = filepath1;
-        filepath2Value = filepath2;
-    })
     .option('-f, --format [type]', 'output format')
+    .arguments('<filepathhh1> <filepathhh2>')
+    .action(function (filepathhh1, filepathhh2) {
+        filepath1Value = filepathhh1;
+        filepath2Value = filepathhh2;
+    })
     .parse(process.argv);
 
 console.log(program.args);
