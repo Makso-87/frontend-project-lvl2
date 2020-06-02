9de64ad7c77f555e8225915bd465008d03b8aba4
Makso-87 <makso16108@gmail.com>
2020-06-02 10:52:17 +0300

    Добавил аргументы и поцию формата в gendiff

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,6 +5,12 @@ import { program } from 'commander';
 program
     .version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
+    .arguments('<filepath1> <filepath2>')
+    .action(function (filepath1, filepath2) {
+        filepath1Value = filepath1;
+        filepath2Value = filepath2;
+    })
+    .option('-f, --format [type]', 'output format')
     .parse(process.argv);
 
 console.log(program.args);
