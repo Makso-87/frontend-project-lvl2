f101c5da956e8621b52c57d905de9aefc6dbc240
Makso-87 <makso16108@gmail.com>
2020-06-11 11:04:01 +0300

    Вынес функции accString и addNewKeys на уровень модуля

--- src/getTwoFilesDifference.js
+++ src/getTwoFilesDifference.js
@@ -3,50 +3,54 @@ import fs from 'fs';
 import path from 'path';
 import _ from 'lodash';
 
+const accString = (arg1, arg2, acc, keyNum = 0) => {
+  const file1 = arg1;
+  const file2 = arg2;
+  const keys = Object.keys(file1);
+  const key = keys[keyNum];
+
+  if (keyNum > keys.length - 1) {
+    return acc;
+  }
+
+  if (_.has(file2, key)) {
+    if (file1[key] === file2[key]) {
+      const newString = `${acc}\n    ${key}: ${file1[key]}`;
+      return accString(file1, file2, newString, keyNum + 1);
+    }
+
+    if (file1[key] !== file2[key]) {
+      const tempString = `${acc}\n  + ${key}: ${file2[key]}`;
+      const newString = `${tempString}\n  - ${key}: ${file1[key]}`;
+      return accString(file1, file2, newString, keyNum + 1);
+    }
+  }
+
+  const newString = `${acc}\n  - ${key}: ${file1[key]}`;
+  return accString(file1, file2, newString, keyNum + 1);
+};
+
+const addNewKeys = (arg1, arg2, acc, keyNum = 0) => {
+  const file1 = arg1;
+  const file2 = arg2;
+  const keys = Object.keys(file2);
+  const key = keys[keyNum];
+
+  if (keyNum > keys.length - 1) {
+    const newString = `${acc}\n}`;
+    return newString;
+  }
+
+  const newString = (_.has(file1, key)) ? acc : `${acc}\n  + ${key}: ${file2[key]}`;
+
+  return addNewKeys(file1, file2, newString, keyNum + 1);
+};
+
 const getDiff = (filepath1, filepath2) => {
   const file1 = JSON.parse(fs.readFileSync(path.resolve(filepath1)));
   const file2 = JSON.parse(fs.readFileSync(path.resolve(filepath2)));
-  const keys1 = Object.keys(file1);
-  const keys2 = Object.keys(file2);
 
-  const accString = (acc, keyNum = 0) => {
-    if (keyNum > keys1.length - 1) {
-      return acc;
-    }
-
-    const key = keys1[keyNum];
-
-    if (_.has(file2, key)) {
-      if (file1[key] === file2[key]) {
-        const newString = `${acc}\n    ${key}: ${file1[key]}`;
-        return accString(newString, keyNum + 1);
-      }
-
-      if (file1[key] !== file2[key]) {
-        const tempString = `${acc}\n  + ${key}: ${file2[key]}`;
-        const newString = `${tempString}\n  - ${key}: ${file1[key]}`;
-        return accString(newString, keyNum + 1);
-      }
-    }
-
-    const newString = `${acc}\n  - ${key}: ${file1[key]}`;
-    return accString(newString, keyNum + 1);
-  };
-
-  const addNewKeys = (acc, keyNum = 0) => {
-    if (keyNum > keys2.length - 1) {
-      const newString = `${acc}\n}`;
-      return newString;
-    }
-
-    const key = keys2[keyNum];
-
-    const newString = (_.has(file1, key)) ? acc : `${acc}\n  + ${key}: ${file2[key]}`;
-
-    return addNewKeys(newString, keyNum + 1);
-  };
-
-  return addNewKeys(accString('{'));
+  return addNewKeys(file1, file2, accString(file1, file2, '{'));
 };
 
 const getFilesDifference = () => {
