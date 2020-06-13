b63078ec191ed5a0563f86d790667d464f8a7cd3
Makso-87 <makso16108@gmail.com>
2020-06-13 12:03:59 +0300

    Сменил тип окончания строки с CRLF на LF во всех файлах

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,70 +2,34 @@ import fs from 'fs';
 import path from 'path';
 import getDiff from '../src/index';
 
-// const getFixturePath = (filename) => path.resolve(path.join('__fixtures__', filename));
-// const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-// let file1;
-// let file2;
+const getFixturePath = (filename) => path.resolve(path.join('__fixtures__', filename));
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+let file1;
+let file2;
 
-// beforeAll(() => {
-//   file1 = readFile('before-after-result.txt');
-//   file2 = readFile('first-second-result.txt');
-// });
+beforeAll(() => {
+  file1 = readFile('before-after-result.txt');
+  file2 = readFile('first-second-result.txt');
+});
 
 test('gendiff-json', () => {
   const string = getDiff('__fixtures__/before.json', '__fixtures__/after.json');
 
-  // expect(string).toBe(file1);
-  expect(string).toEqual('{\
-\n    host: hexlet.io\
-\n  + timeout: 20\
-\n  - timeout: 50\
-\n  - proxy: 123.234.53.22\
-\n  + follow: true\
-\n  - follow: false\
-\n  + verbose: true\
-\n}');
+  expect(string).toEqual(file1);
 
   const newString = getDiff('__fixtures__/first.json', '__fixtures__/second.json');
 
-  expect(newString).toEqual('{\
-\n  - first-key: value1\
-\n  + second-key: false\
-\n  - second-key: true\
-\n  + coconut: 80\
-\n  - coconut: 60\
-\n    milk: white\
-\n  + car: Tesla\
-\n  + bond: James Bond\
-\n}');
+  expect(newString).toEqual(file2);
 });
 
 test('gendiff-yaml', () => {
   const string = getDiff('__fixtures__/before.yml', '__fixtures__/after.yml');
 
-  // expect(string).toBe(file1);
-  expect(string).toEqual('{\
-\n    host: hexlet.io\
-\n  + timeout: 20\
-\n  - timeout: 50\
-\n  - proxy: 123.234.53.22\
-\n  + follow: true\
-\n  - follow: false\
-\n  + verbose: true\
-\n}');
+  expect(string).toEqual(file1);
 });
 
 test('gendiff-ini', () => {
   const string = getDiff('__fixtures__/before.ini', '__fixtures__/after.ini');
 
-  // expect(string).toBe(file1);
-  expect(string).toEqual('{\
-\n    host: hexlet.io\
-\n  + timeout: 20\
-\n  - timeout: 50\
-\n  - proxy: 123.234.53.22\
-\n  + follow: true\
-\n  - follow: false\
-\n  + verbose: true\
-\n}');
+  expect(string).toEqual(file1);
 });
