28ebde67f8d62393adf4e2da4be437cfe05d6727
Makso-87 <makso16108@gmail.com>
2020-08-23 21:55:54 +0300

    Исправил код по требованию линтера

--- src/formaters/plain.js
+++ src/formaters/plain.js
@@ -1,24 +1,19 @@
 import _ from 'lodash';
 
 const toStringPlainStyle = (tree, path = []) => {
-  const resultString = [];
-  tree.map((key) => {
+  const result = tree.flatMap((key) => {
     if (key.status === 'nested') {
       path.push(`${key.name}.`);
-      resultString.push(toStringPlainStyle(key.children, path));
+      const newPath = [...path];
       path.pop();
-    }
-
-    if (key.status === 'removed') {
-      const pathString = `${path.join('')}${key.name}`;
-      resultString.push(`Property '${pathString}' was deleted`);
+      return toStringPlainStyle(key.children, newPath);
     }
 
     if (key.status === 'added') {
       const pathString = `${path.join('')}${key.name}`;
       const value = _.isObject(key.value) ? '[complex value]' : key.value;
       const typingValue = _.isBoolean(value) || _.isNumber(value) || value === '[complex value]' ? value : `'${key.value}'`;
-      resultString.push(`Property '${pathString}' was added with value: ${typingValue}`);
+      return `Property '${pathString}' was added with value: ${typingValue}`;
     }
 
     if (key.status === 'changed') {
@@ -27,11 +22,18 @@ const toStringPlainStyle = (tree, path = []) => {
       const typingOldValue = _.isBoolean(oldValue) || _.isNumber(oldValue) || oldValue === '[complex value]' ? oldValue : `'${key.oldValue}'`;
       const newValue = _.isObject(key.newValue) ? '[complex value]' : key.newValue;
       const typingNewValue = _.isBoolean(newValue) || _.isNumber(newValue) || newValue === '[complex value]' ? newValue : `'${key.newValue}'`;
-      resultString.push(`Property '${pathString}' was changed from ${typingOldValue} to ${typingNewValue}`);
+      return `Property '${pathString}' was changed from ${typingOldValue} to ${typingNewValue}`;
     }
+
+    if (key.status === 'removed') {
+      const pathString = `${path.join('')}${key.name}`;
+      return `Property '${pathString}' was deleted`;
+    }
+
+    return [];
   });
 
-  return resultString.flat().join('\n');
+  return result.join('\n');
 };
 
 export default toStringPlainStyle;
